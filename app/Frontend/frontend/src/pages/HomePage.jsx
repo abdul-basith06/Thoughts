52a8bfb8b465e12c070f428f38ce16718fdf3d12
abdul-basith06 <basitha85@gmail.com>
2024-05-20 22:35:36 +0530

    Implemented Navbar into the homepage

--- app/Frontend/frontend/src/pages/HomePage.jsx
+++ app/Frontend/frontend/src/pages/HomePage.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import Navbar from '../components/Navbar'
 
 const HomePage = () => {
   return (
     <div>
-        <h1>Home Pgaeeee</h1>
+      <div className='bg-slate-900'>
+        <Navbar />
+      </div>
+     <div className="p-6 max-w-3xl mx-auto space-y-6">
+      {/* <CreateThought />
+      <ListThoughts /> */}
+     </div>
     </div>
   )
 }
