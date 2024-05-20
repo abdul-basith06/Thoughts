4098f2ce4118cd9abd2e9ed478eb1bde289fe7c1
abdul-basith06 <basitha85@gmail.com>
2024-05-20 21:21:40 +0530

    User registration setup

--- app/Backend/backend/api/urls.py
+++ app/Backend/backend/api/urls.py
@@ -5,7 +5,7 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 urlpatterns = [
     path("token/", TokenObtainPairView.as_view(), name="get_token"),
     path("token/refresh/", TokenRefreshView.as_view(), name="refresh_token"),
-    # path('register/', RegisterView.as_view()),
+    path('register/', RegisterView.as_view()),
     # path("user/profile/", UserUpdateProfile.as_view(), name="user-profile"),
     # path("thoughts/", ThoughtsCreateListView.as_view(), name="thoughts-create-list"),
 ]
--- app/Backend/backend/api/views.py
+++ app/Backend/backend/api/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
-
+from rest_framework.response import Response
+from rest_framework import generics, status
+from rest_framework.views import APIView
+from rest_framework.generics import (
+    ListCreateAPIView,
+    RetrieveUpdateDestroyAPIView,
+    UpdateAPIView,
+    RetrieveAPIView,
+)
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer # type: ignore
+from rest_framework_simplejwt.views import TokenObtainPairView  # type: ignore
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from .serializers import *
 # Create your views here.
+
+class RegisterView(APIView):
+    def post(self,request):
+        serializer=UserSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
