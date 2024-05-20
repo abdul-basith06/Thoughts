26d98d89eb46dc04e96a93dd2017086245c39ea1
abdul-basith06 <basitha85@gmail.com>
2024-05-20 22:02:14 +0530

    Signin and Signup

--- app/Frontend/frontend/src/App.jsx
+++ app/Frontend/frontend/src/App.jsx
@@ -1,6 +1,12 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import RegisterPage from "./pages/RegisterPage";
 import LoginPage from "./pages/LoginPage";
+import HomePage from "./pages/HomePage";
+
+function Logout() {
+  localStorage.clear()
+  return <Navigate to="/login" />
+}
 
 function RegisterAndLogout() {
   localStorage.clear();
@@ -14,6 +20,8 @@ function App() {
         <Routes>
           <Route path="/login" element={<LoginPage />} />
           <Route path="/register" element={<RegisterAndLogout />} />
+          <Route path="/logout" element={<Logout />}/>
+          <Route path="/" element={<HomePage />}/>
         </Routes>
       </BrowserRouter>
     </>
--- app/Frontend/frontend/src/components/Login.jsx
+++ app/Frontend/frontend/src/components/Login.jsx
@@ -1,12 +1,30 @@
 import React, { useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-// import login, { getlocal } from "../helpers/auth";
-// import { useDispatch, useSelector } from "react-redux";
-// import { jwtDecode } from "jwt-decode";
-// import { updateToken, updateUser } from "../features/UserSlice";
 import toast, { Toaster } from "react-hot-toast";
+import { ACCESS_TOKEN, REFRESH_TOKEN } from "../constants";
+import api from "../api";
 
 const Login = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  //   const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+  const route="/api/token/"
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const res = await api.post(route, { username, password });
+      console.log("res---------",res);
+      localStorage.setItem(ACCESS_TOKEN, res.data.access);
+      localStorage.setItem(REFRESH_TOKEN, res.data.refresh);
+      navigate("/");
+    } catch (error) {
+      alert(error);
+    }
+  };
+
   return (
     <div
       className={
@@ -18,12 +36,24 @@ const Login = () => {
         <h2 className="text-3xl font-medium mb-4 text-black dark:text-white text-center">
           Login
         </h2>
-        <form >
+        <form onSubmit={handleSubmit}>
           <div className="mb-4">
-            <input type="text" placeholder="username" name="username" />
+            <input
+              type="text"
+              placeholder="username"
+              name="username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
           </div>
           <div className="mb-4">
-            <input type="password" placeholder="password" name="password" />
+            <input
+              type="password"
+              placeholder="password"
+              name="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
           </div>
 
           <button className="sbmt" type="submit">
@@ -38,7 +68,7 @@ const Login = () => {
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
